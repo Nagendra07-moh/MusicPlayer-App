aee682aebda4aa7826889caa22867755dbb32430
Nagendra07-moh <nagendra.mohan.5@gmail.com>
2024-06-12 21:08:37 +0530

    all good

--- src/app/(tabs)/(songs)/index.tsx
+++ src/app/(tabs)/(songs)/index.tsx
@@ -1,4 +1,5 @@
 import { TracksList } from '@/app/components/TracksList'
+import { screenPadding } from '@/app/constants/tokens'
 import { defaultStyles } from '@/app/styles'
 
 import { View, Text, ScrollView } from 'react-native'
@@ -6,7 +7,10 @@ import { View, Text, ScrollView } from 'react-native'
 const SongsScreen = () => {
 	return (
 		<View style={defaultStyles.container}>
-			<ScrollView>
+			<ScrollView
+				contentInsetAdjustmentBehavior="automatic"
+				style={{ paddingHorizontal: screenPadding.horizontal }}
+			>
 				<TracksList scrollEnabled={false} />
 			</ScrollView>
 		</View>
